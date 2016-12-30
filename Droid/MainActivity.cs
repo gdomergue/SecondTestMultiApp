7a21a862e2b9264396e7c16bfdb64bbe856315e8
gdomergue <gdomergu@callutheran.edu>
2016-12-30 00:29:56 -0800

    Enabled Google Maps for Android. Not fully completed yet; the map content doesn’t display yet.

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -1,13 +1,17 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Gms.Maps;
+using System;
+using Android;
+using Android.Support.V4.App;
 
 namespace SecondTestMultiApp.Droid
 {
-	[Activity(Label = "SecondTestMultiApp", MainLauncher = true, Icon = "@mipmap/icon")]
-	public class MainActivity : Activity
+	[Activity(Label = "SecondTestMultiApp", MainLauncher = true)]
+	public class MainActivity : Activity, IOnMapReadyCallback
 	{
-		int count = 1;
+		private GoogleMap mMap;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -15,12 +19,30 @@ namespace SecondTestMultiApp.Droid
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+			SetUpMap();
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+		}
 
-			button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+		private void SetUpMap()
+		{
+			if (mMap == null)
+			{
+				FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+				// Note: Resource.Id.map is the Id that we named the map in the MapLayout.axml
+
+			}
+		}
+
+		/*
+		 * When Map is ready to display from the "GetMapAsync(this)" call, we will assign 
+		 * our map value to be the GoogleMap this method returns.
+		 * 
+		 * GoogleMap value is guaranteed to be non-null.
+		 */
+		public void OnMapReady(GoogleMap googleMap)
+		{
+			mMap = googleMap;
+			//mMap.mylo
 		}
 	}
 }
---
+++ Droid/MapActivity.cs
@@ -0,0 +1,95 @@
+﻿
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+
+//using Android.Content;
+
+//using Android.Runtime;
+//using Android.Views;
+
+
+namespace SecondTestMultiApp.Droid
+{
+	using System;
+	using Android.App;
+	using Android.Gms.Maps;
+	using Android.Gms.Maps.Model;
+	using Android.OS;
+	using Android.Widget;
+
+	[Activity(Label = "MapActivity")]
+	public class MapActivity : Activity, IOnMapReadyCallback
+	{
+		private static readonly LatLng Passchendaele = new LatLng(50.897778, 3.013333);
+		private static readonly LatLng VimyRidge = new LatLng(50.379444, 2.773611);
+		private GoogleMap _map;
+		private MapFragment _mapFragment;
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Create your application here
+			SetContentView(Resource.Layout.MapLayout);
+			InitMapFragment();
+
+		}
+
+		protected override void OnResume()
+		{
+			base.OnResume();
+			//CreateMapIfNeeded();
+		}
+
+		private void InitMapFragment()
+		{
+			_mapFragment = FragmentManager.FindFragmentByTag("map") as MapFragment;
+			if (_mapFragment == null)
+			{
+				GoogleMapOptions mapOptions = new GoogleMapOptions()
+					.InvokeMapType(GoogleMap.MapTypeSatellite)
+					.InvokeZoomControlsEnabled(false)
+					.InvokeCompassEnabled(true);
+
+				FragmentTransaction fragTx = FragmentManager.BeginTransaction();
+				_mapFragment = MapFragment.NewInstance(mapOptions);
+				fragTx.Add(Resource.Id.map, _mapFragment, "map");
+				fragTx.Commit();
+			}
+		}
+
+		private void CreateMapIfNeeded()
+		{
+			if (_map == null)
+			{
+				FragmentManager.FindFragmentById<MapFragment>(Resource.Id.map).GetMapAsync(this);
+
+				if (_map != null)
+				{
+					//MarkerOptions markerOpt1 = new MarkerOptions();
+					//markerOpt1.SetPosition(VimyRidge);
+					//markerOpt1.SetTitle("Vimy Ridge");
+					//markerOpt1.InvokeIcon(BitmapDescriptorFactory.DefaultMarker(BitmapDescriptorFactory.HueCyan));
+					//_map.AddMarker(markerOpt1);
+
+					//MarkerOptions markerOpt2 = new MarkerOptions();
+					//markerOpt2.SetPosition(Passchendaele);
+					//markerOpt2.SetTitle("Passchendaele");
+					//_map.AddMarker(markerOpt2);
+
+					//// We create an instance of CameraUpdate, and move the map to it.
+					//CameraUpdate cameraUpdate = CameraUpdateFactory.NewLatLngZoom(VimyRidge, 15);
+					//_map.MoveCamera(cameraUpdate);
+				}
+			}
+		}
+
+		public void OnMapReady(GoogleMap googleMap)
+		{
+			_map = googleMap;
+		}
+	}
+}
